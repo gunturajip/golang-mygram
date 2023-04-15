3c0459426589e87d1c0c0ab1e3bcf5bfdf77650d
gunturajip <gunturaji852@gmail.com>
2023-04-15 11:32:07 +0700

    [Model] fix password usability

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 import (
+	"errors"
 	"golang-mygram/helpers"
-	"strconv"
 
 	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
@@ -13,8 +13,8 @@ type User struct {
 	GormModel
 	Username string `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Your username is required"`
 	Email    string `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Your email is required, email~Invalid email format"`
-	Password string `gorm:"not null" json:"-" form:"password" valid:"required~Your password is required, minstringlength(6)~Password has to have a minimum length of 6 characters"`
-	Age      int    `gorm:"not null" json:"age" form:"age" valid:"required~Your age is required, numeric~Age must be numeric, age~Age has to more than 8"`
+	Password string `gorm:"not null" json:"password" form:"password" valid:"required~Your password is required, minstringlength(6)~Password has to have a minimum length of 6 characters"`
+	Age      int    `gorm:"not null" json:"age" form:"age" valid:"required~Your age is required, numeric~Age must be numeric"`
 	// Admin        bool          `gorm:"not null;default:0" json:"admin" form:"admin"`
 	Photos       []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"-"`
 	Socialmedias []Socialmedia `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"-"`
@@ -22,15 +22,15 @@ type User struct {
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
-	govalidator.TagMap["age"] = govalidator.Validator(func(str string) bool {
-		age, _ := strconv.Atoi(str)
-		return age > 8
-	})
 	_, errCreate := govalidator.ValidateStruct(u)
 
 	if errCreate != nil {
 		err = errCreate
 		return err
+	}
+
+	if u.Age <= 8 {
+		return errors.New("you must be over 8 years old")
 	}
 
 	u.Password = helpers.HashPass(u.Password)
